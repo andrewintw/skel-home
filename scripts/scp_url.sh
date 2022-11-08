73de5b1ac43409c09e2dc48bf300f2926cbb3036
Andrew <yenchang.lin@gmail.com>
2022-11-08 18:30:46 +0800

    Add files via upload

---
+++ scripts/scp_url.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+scppath="$1"
+
+if [ "$scppath" = "" ]; then
+	target="$PWD"
+else
+	target=`realpath $scppath`
+fi
+
+if [ ! -e "$target" ]; then
+	echo "$target is NOT exist"
+	exit 1
+fi
+
+if=`route -n | grep -w UG | awk  '{print $8}'`
+ip=`ifconfig $if | grep -w inet | awk '{print $2}'`
+path=`readlink -f $target`
+
+echo "`whoami`@$ip:$path"
